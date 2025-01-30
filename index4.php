b71e036618f38718ce25eaa1a83fe0b06afd2e6f
kubek519 <bobrownik023@gmail.com>
2025-01-30 22:14:27 +0100

    Add files via upload

---
+++ index4.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styl.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zadanie 3.5</title>
+    
+</head>
+<body>
+    <form method="post" action="index2.php">
+
+    <label for="imie">Imię</label>
+    <input type="text" name="imie">
+    <label for="nazwisko">Nazwisko</label>
+    <input type="text" name="nazwisko">
+    <label for="ocena">Ocena</label>
+    <input type="number" name="ocena">
+    <label for="wiek">Wiek</label>
+    <input type="number" name="wiek">
+    <label for="id">id</label>
+    <input type="number" name="id">
+
+    <input type="submit" value="Dodaj Ucznia">
+
+
+    </form>
+</body>
+</html>
